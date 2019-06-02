fdaff71f64a8328290c6128de581fcb3aa4e7f84
omerHof <54277477h>
2019-06-02 14:55:10 +0300

    new game page update2

--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -1,12 +1,22 @@
 package View;
 
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 import java.sql.SQLOutput;
 
 public class MyViewController implements IView {
 
+    /*
+    @FXML
+    private ViewModel viewModel;
+    public MazeDisplayer mazeDisplayer;
 
+    public void setViewModel(ViewModel viewModel) {
+        this.viewModel = viewModel;
+        bindProperties(viewModel);
 
+    }
+    */
 }
