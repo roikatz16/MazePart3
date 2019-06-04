2859d89e37872cc1849d0d76812acf7ec77e6e01
omerHof <54277477h>
2019-06-04 10:54:21 +0300

    model update2

--- src/ViewModel/MyViewModel.java
+++ src/ViewModel/MyViewModel.java
@@ -34,7 +34,7 @@ public class MyViewModel extends Observable implements Observer {
     @Override
     public void update(Observable o, Object arg) {
         if (o==model){
-
+            System.out.println("workkk");
             mazeAsArray = (model.getMaze()).getArray();//possible not update charArray every time?
             rowStartPosition = (model.getMaze()).getStartPosition().getRowIndex() ;
             colStartPosition = (model.getMaze()).getStartPosition().getColumnIndex() ;
